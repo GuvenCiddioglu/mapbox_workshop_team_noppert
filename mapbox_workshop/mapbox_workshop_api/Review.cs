d06ff2880142e7da735112187052de627a78b359
Troy98 <Gauvun@hotmail.com>
2024-04-26 11:47:22 +0200

    AirBNB db scaffolded

---
+++ mapbox_workshop/mapbox_workshop_api/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mapbox_workshop_api;
+
+public partial class Review
+{
+    public long ListingId { get; set; }
+
+    public DateOnly Date { get; set; }
+}
